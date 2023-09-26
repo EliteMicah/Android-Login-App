af44fca3e0af66d44d33439cb19fafc9fdd184b0
Micah Woodring <84886282+EliteMicah@users.noreply.github.com>
2023-09-25 20:21:20 -0700

    Update Activity_display_message.java

--- Activity_display_message.java
+++ Activity_display_message.java
@@ -1,6 +1,8 @@
 package com.example.rebooked1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,13 +14,16 @@ public class Activity_display_message extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
+        androidx.appcompat.widget.Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         // Retrieve the first and last names from the intent
         Intent intent = getIntent();
         String messageFirstName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_FIRST);
         String messageLastName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_LAST);
 
         // Create a welcome message
-        String welcomeMessage = "The following contact has been added!\n" + messageFirstName + " " + messageLastName + "!";
+        String welcomeMessage = "The following contact has been added! \n" + messageFirstName + " " + messageLastName + "!";
 
         // Display the welcome message in a TextView or another UI element
         TextView textViewWelcome = findViewById(R.id.contactAddedText);
